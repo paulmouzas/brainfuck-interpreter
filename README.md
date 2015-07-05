e55d6ea14fbab8f7a42048f23a76305b99fbcf28
paul <paul.mouzas@gmail.com>
2015-07-05 12:32:04 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Brainfuck interpreter in python.
---
+++ brainfuck.py
@@ -0,0 +1,73 @@
+import sys
+
+
+class Interpreter(object):
+
+    def __init__(self, filename, n=5000):
+        with open(filename, 'r') as f:
+            self.text = self.strip(f.read())
+        self.data = [0 for x in range(n)]  # not infinite but that's OK
+        self.dp = 0  # data pointer
+        self.cp = 0  # code pointer
+        self.jump = [None for x in range(n)] #  keeps track where to jump to in loops
+        self.loop_track(self.text)
+
+    def loop_track(self, text):
+        stack = []
+        for i, char in enumerate(text):
+            if char == '[':
+                stack.append(i)
+            elif char == ']':
+                self.jump[i] = stack.pop()
+                self.jump[self.jump[i]] = i
+
+    def strip(self, text):
+        # remove all whitespace and non-keyword characters
+        allowed = '<>-+.,[]'
+        return ''.join([x for x in text if x in allowed])
+
+    def increment(self):
+        self.data[self.dp] += 1
+
+    def decrement(self):
+        self.data[self.dp] -= 1
+    
+    def move_left(self):
+        self.dp -= 1
+
+    def move_right(self):
+        self.dp += 1
+
+    def print_char(self):
+        sys.stdout.write(chr(self.data[self.dp]))
+
+    def input_char(self):
+        self.data[self.dp] = ord(sys.stdin.read(1))
+
+    def eval(self):
+        while self.cp < len(self.text) - 1:
+            char = self.text[self.cp]
+            if char == '+':
+                self.increment()
+            elif char == '-':
+                self.decrement()
+            elif char == '>':
+                self.move_right()
+            elif char == '<':
+                self.move_left()
+            elif char == '.':
+                self.print_char()
+            elif char == ',':
+                self.input_char()
+            elif char == '[':
+                if not self.data[self.dp]:
+                    self.cp = self.jump[self.cp]
+            elif char == ']':
+                if self.data[self.dp]:
+                    self.cp = self.jump[self.cp]
+            self.cp += 1
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    bf = Interpreter(filename)
+    bf.eval()
